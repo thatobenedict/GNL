15a75ee4b2674328f2544d0b3535ce337b9d2194
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-06-08 14:52:39 +0200

    first push

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/08 13:05:38 by tbenedic          #+#    #+#             */
+/*   Updated: 2018/06/08 14:49:46 by tbenedic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#define BUFF_SIZE 32
+#include "../libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
