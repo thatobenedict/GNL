fa9604b93e6cb794ab35c3b80862daf3fe5780ef
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-06-18 16:19:17 +0200

    a main

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,53 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 13:05:22 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/06/08 13:45:14 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/06/18 16:14:56 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h> //for the main
+#include <sys/types.h> //for the main
+#include <sys/types.h> //for the main
+#include <fcntl.h>
+
+
+const int	in_line(char **line, )
+{
+
+
+
+}
+/*
 int get_next_line(const int fd, char **line)
 {
 
+
+}
+*/
+int		main(void)
+{
+	int fd;
+	char buff[BUFF_SIZE + 1];
+	size_t bufout;
+
+	fd = open("file.txt", O_RDONLY);
+	if(fd == -1)
+	{
+		printf("Failed to create and open and read the file.\n");
+		return (1);
+	}
+
+	file_des()
+ 	bufout = read(fd, buff, BUFF_SIZE);
+	buff[BUFF_SIZE] = '\0';
+
+	printf("%d\n", ft_strlen(buff));
+	printf("%zu\n", bufout);
+	printf("buf is: \n%s\n", buff);
+	close(fd);
+	return 0;
 }
