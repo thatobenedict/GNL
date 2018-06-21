f54b55eba02a8adb21994c75c08353264219d4f0
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-06-21 16:42:47 +0200

    end of day

--- get_next_linev2.c
+++ get_next_linev2.c
@@ -6,7 +6,7 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 13:05:22 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/06/21 16:12:56 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/06/21 16:42:25 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,15 @@ int		get_next_line(const int fd, char **line)
 	char		*buf;
 	char		*temp;
 	size_t		buffout;
-	static int	i;
-	static int	j;
+	int			i = 0;
 
-
-	buf = ft_strnew(BUFF_SIZE);
 	while (buffout > 0)
 	{
+		buf = ft_strnew(BUFF_SIZE);
 		buffout = read(fd, buf, BUFF_SIZE);
-		j = i;
-		while (buf[i] != '\0' && buf[i] != '\n')
-			i++;
-		temp = ft_strsub(buf, j, i - j);
-		printf("buf is: \n%s\n", temp);
+		temp = ft_strdup(buf);
+		printf("buf %d is: \n%s\n",i ,temp);
+		i++;
 	}
 	if (temp == NULL)
 		return (0);
@@ -47,9 +43,8 @@ int		main(void)
 {
 	int		fd;
 	int		strbuf;
-	//	char	*strbuf;
 	char	*line;
-	int i = 0;
+	int 	i = 0;
 
 	fd = open("file.txt", O_RDONLY, 0700);
 	if(fd == -1)
@@ -57,7 +52,7 @@ int		main(void)
 		printf("Failed to create and open and read the file.\n");
 		return (1);
 	}
-	while ((strbuf = get_next_line(fd, &line)) == 1 && i < 5 )
+	while ((strbuf = get_next_line(fd, &line)) == 1 && i < 10)
 	{
 		//	printf("fd is:\n%d\n", fd);
 		//	printf("return:\n%d\n", strbuf);
