9f2ba55161ebe710170b119d208ed734189f2f51
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-06-25 18:01:48 +0200

    Monday EOD

--- get_next_linev2.c
+++ get_next_linev2.c
@@ -6,7 +6,7 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 13:05:22 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/06/23 15:58:58 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/06/25 18:00:53 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,32 @@
 #include <sys/types.h> //for the main
 #include <fcntl.h>
 
-size_t		read_line(char *buf)
+static size_t		read_line(char *buf)
 {
 	size_t i;
 
 	i = 0;
-	while (buf[i] != '\n' && !buf)
+	while (buf[i] != '\n' && buf[i] != '\0')
 		i++;
 	return (i);
+}
+
+static char		*ft_change(char *line)
+{
+	char	*recycle;
+
+	recycle = ft_strchr(line, '\n');
+	return (recycle);
+}
+
+static size_t		ft_change_size(char *found)
+{
+	size_t len;
+	const char *str;
+
+   	str = ft_change(found);
+	len = ft_strlen(str);
+	return (len);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -34,8 +52,9 @@ int		get_next_line(const int fd, char **line)
 	static char	*temp;
 	size_t		buffout;
 	int			i = 0;
+	char		*test;
 
-	temp = "";	
+	temp = "";
 	buf = ft_strnew(BUFF_SIZE);
 	while (/*BUFF_SIZE > 0 ||*/ i < 5)
 	{
@@ -43,12 +62,19 @@ int		get_next_line(const int fd, char **line)
 		buf = ft_strnew(BUFF_SIZE);
 		buffout = read(fd, buf, BUFF_SIZE);
 		if (ft_contain_char(buf, '\n') == 0)
-			temp = ft_strnjoin(temp, buf, BUFF_SIZE);
+			temp = ft_strjoin(temp, buf);
 		else
 		{
 			*line = ft_strnjoin(temp, buf, read_line(buf));
-			*line = temp;
+			printf("DELETE! The length of line is |%d|\n", ft_strlen(*line));
+			printf("DELETE! The length of read_line(buf) is |%zu|\n", read_line(buf));
+			printf("DELETE! (buf) is |%s|\n", buf);
+			//*line = ft_strsub(*line, *line[0], read_line(*line));
+			free(temp);
+			test = ft_strnew(ft_change_size(*line));
+			test = ft_change(*line);
 			printf("line is: \n%s\n", *line);
+			printf("test is: \n%s\n", test);
 			return (READ_SUCCESS);
 		}
 		i++; //for the print out
