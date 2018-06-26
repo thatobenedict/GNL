7af40a6ad6528779dda874b01586bbab28fb7452
Thato BENEDICT <tbenedic@e4r20p4.wethinkcode.co.za>
2018-06-26 18:59:16 +0200

    the legend Z

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tbenedic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 13:05:22 by tbenedic          #+#    #+#             */
-/*   Updated: 2018/06/26 16:48:20 by tbenedic         ###   ########.fr       */
+/*   Updated: 2018/06/26 18:58:10 by tbenedic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <sys/types.h> //for the main
 #include <fcntl.h>
 
-static size_t		read_line(char *buf)
+static size_t		new_line_ind(char *buf)
 {
 	size_t i;
 
@@ -34,66 +34,47 @@ static char		*ft_change(char *line)
 
 	recycle = ft_strchr(line, '\n');
 	recycle++;
+	recycle = ft_strdup(recycle);
 	return (recycle);
 }
+/*
+   static size_t		ft_change_size(char *found)
+   {
+   size_t len;
+   const char *str;
 
-static size_t		ft_change_size(char *found)
-{
-	size_t len;
-	const char *str;
-
-   	str = ft_change(found);
-	len = ft_strlen(str);
-	return (len);
-}
-
+   str = ft_change(found);
+   len = ft_strlen(str);
+   return (len);
+   }
+   you're using a strdup now - probably will delete*/
 int		get_next_line(const int fd, char **line)
 {
 	char		*buf;
-	static char	*temp;
+	static char	*keep;
 	size_t		buffout;
-	int			i = 0;
-	char		*hold;
 
-	if (temp == NULL)
-		temp = "";
+	if (keep == NULL)
+		keep = ft_strnew(0);
 	buf = ft_strnew(BUFF_SIZE);
-	while (BUFF_SIZE > 0)
+	while ((buffout = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		printf("buf %d is: \n|%s|\n",i ,temp);
-		buf = ft_strnew(BUFF_SIZE);
-		buffout = read(fd, buf, BUFF_SIZE);
-		if (ft_contain_char(buf, '\n') == 0)
-			temp = ft_strjoin(temp, buf);
-		else
-		{
-			hold = ft_strjoin(temp, buf);
-			*line = ft_strnjoin(temp, buf, read_line(buf));
-			printf("DELETE! The length of line is |%d|\n", ft_strlen(*line));
-			printf("DELETE! The length of read_line(buf) is |%zu|\n", read_line(buf));
-			printf("DELETE! (buf) is |%s|\n", buf);
-			free(temp);
-			temp = ft_strnew(ft_change_size(hold));
-			temp = ft_change(hold);
-			printf("\nline is: \n|%s|\n", *line);
-			printf("temp is: \n|%s|\n", temp);
-			if (temp == NULL)
-				return (READ_COMPLETE);
-			return (READ_SUCCESS);
-		}
-		i++; //for the print out
+		keep = ft_strjoin(keep, buf);
+		if (ft_contain_char(keep, '\n'))
+			break ;
 	}
-	if (temp == NULL)
-		return (READ_ERROR);
-	return (READ_COMPLETE);
+	*line = ft_strsub(keep, 0, new_line_ind(keep));
+	keep = ft_change(keep);
+	if (*keep == 0 && buffout == 0)
+		return (READ_COMPLETE);
+	return (READ_SUCCESS);
 }
-
+/*
 int		main(void)
 {
 	int		fd;
 	int		strbuf;
 	char	*line;
-	int 	i = 0;
 
 	fd = open("file.txt", O_RDONLY, 0700);
 	if(fd == -1)
@@ -101,13 +82,11 @@ int		main(void)
 		printf("Failed to create and open and read the file.\n");
 		return (1);
 	}
-	while ((strbuf = get_next_line(fd, &line)) == 0)
-	{
-		//	printf("fd is:\n%d\n", fd);
-		//	printf("return:\n%d\n", strbuf);
-		i++;
-	}
+	strbuf = get_next_line(fd, &line);
+		printf("%s", line);
+	strbuf = get_next_line(fd, &line);
+	printf("%s", line);
 	close(fd);
 	return 0;
 
-}
+}*/
